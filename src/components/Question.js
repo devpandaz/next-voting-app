04a3c288fc316c843316f9a26565fc68ca07adf4
Jack <superjackxh@gmail.com>
2023-08-31 01:31:36 +0800

    fix lint

--- src/components/Question.js
+++ src/components/Question.js
@@ -73,6 +73,7 @@ export default function Question({ questionId }) {
     return () => {
       pusher_client.unsubscribe(`${questionId}`);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   if (loading || !user || !question) {
